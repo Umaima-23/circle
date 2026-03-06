fe894562d552cea5f9bf88bbc6186632aec6f52e
Umaima <umaimasohail234@gmail.com>
2026-03-06 16:26:30 +0500

    'spacing-solved'

--- src/components/Section1/Hero4.jsx
+++ src/components/Section1/Hero4.jsx
@@ -15,7 +15,7 @@ const Hero4 = () => {
         <div className='h-screen '>
             <div className='mt-30 flex rounded-full justify-center'><Subtitle text='AWARDS' /></div>
             <div className='text-4xl mt-3 text-center'>An <span className='font-[700]'> award winning </span> platform, <span className='font-[700]'> loved by customers.</span> </div>
-            <div className='grid grid-cols-6 gap-10 m-20 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center'>
+            <div className='grid grid-cols-1 gap-x-2 gap-y-10 m-20 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mx-auto max-w-4xl justify-items-center'>
             <Cardh img={one} text='Market leader across 18 categories'/>
             <Cardh img={two} text='Most loved SaaS tool in 2021'/>    
             <Cardh  img={three}  text='Category leader in 2022'/>    
